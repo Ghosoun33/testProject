f9e6ed4a842f7c7d6b3c86dfaa9d589b89d7c056
غدا انسى <go090on@gmail.com>
2019-08-21 14:20:52 +0300

    Day2

---
+++ Day2.py
@@ -0,0 +1,10 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello, World!')
+Hello, World!
+>>> if 5 > 2:
+	print('Five is grater than two!')
+
+
+Five is grater than two!
+>>>
