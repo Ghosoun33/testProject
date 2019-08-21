e7b39853bfb8d6c9be0c00c049e871349ba30ee0
غدا انسى <go090on@gmail.com>
2019-08-21 18:43:10 +0300

    Day3

---
+++ Day3.py
@@ -0,0 +1,24 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #This is a comment
+>>> print('Hello, World!')
+Hello, World!
+>>> print('Hello, World!')#this is a comment
+Hello, World!
+>>> #print('Hello, World!')
+>>> print('Cheers, Mate!')
+Cheers, Mate!
+>>> #This is a comment
+>>> #Written in
+>>> #more than just one line
+>>> print('Hello, World!')
+Hello, World!
+>>> '''
+This is a comment
+Written in
+more than just one line
+'''
+'\nThis is a comment\nWritten in\nmore than just one line\n'
+>>> print('Hello, World!')
+Hello, World!
+>>> 
