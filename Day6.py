dc03f0e064531ffda0223895ccb4b7997cce96e7
غدا انسى <go090on@gmail.com>
2019-08-24 19:10:14 +0300

    Day7

---
+++ Day6.py
@@ -0,0 +1,10 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x, y, z = 'apple', 'orange', 'limon'
+>>> basket = x + y + z
+>>> print(basket)
+appleorangelimon
+>>> basket = x +' '+ y+ ' ' + z
+>>> print(basket)
+apple orange limon
+>>> 
