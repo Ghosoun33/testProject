3d718dd1d6dca1c04696b0c7651458cbc0795847
غدا انسى <go090on@gmail.com>
2019-08-26 12:40:48 +0300

    Day8

---
+++ Day8.py
@@ -0,0 +1,19 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> print('Hello')
+Hello
+>>> a = 'Hello'
+>>> print(a)
+Hello
+>>> a = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'''
+>>> print(a)
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+>>> a = 'Hello, World'
+>>> print(a[1])
+e
+>>> b = 'Hello, World'
+>>> print(b[2:5])
+llo
+>>> 
