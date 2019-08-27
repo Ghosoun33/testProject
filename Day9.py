97b68872f2df43e5551f4a07f2cf8765c31e7e9a
غدا انسى <go090on@gmail.com>
2019-08-27 09:07:21 +0300

    Day9

---
+++ Day9.py
@@ -0,0 +1,21 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = ' Hello, World! '
+>>> print(a.strip())#returns 'Hello, World!'
+Hello, World!
+>>> a = 'Hello, World!'
+>>> print(len(a))
+13
+>>> a = 'Hello, World!'
+>>> print(a.lower())
+hello, world!
+>>> a = 'Hello, World!'
+>>> print(a.upper())
+HELLO, WORLD!
+>>> a = 'Hello, World!'
+>>> print(a.replace('H', 'J'))
+Jello, World!
+>>> a = 'Hello, World!'
+>>> print(a.split(',')) # returns ['Hello', 'World!']
+['Hello', ' World!']
+>>> 
