ff2f7f58868e71e807a1ae73048d469744bff58f
غدا انسى <go090on@gmail.com>
2019-08-29 11:55:51 +0300

    Day11

---
+++ Day11.py
@@ -0,0 +1,26 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> y = 3
+>>> print(x * y)
+15
+>>> print(x + y)
+8
+>>> print(x - y)
+2
+>>> x = 5
+>>> x /= 3
+>>> print(x)
+1.6666666666666667
+>>> x *= 3
+>>> print(x)
+5.0
+>>> x = 5
+>>> y = 3
+>>> print(x < y)
+False
+>>> print(x == y)
+False
+>>> print(x != y)
+True
+>>> 
