fbe9a299ababec55a0c9705dfe17a216d4516e10
غدا انسى <go090on@gmail.com>
2019-08-30 14:26:58 +0300

    Day12

---
+++ Day12.py
@@ -0,0 +1,17 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> print(x > 3 or x < 4)
+True
+>>> x = ['apple', 'banana']
+>>> y = ['apple', 'banana']
+>>> z = x
+>>> print(x is not z)
+False
+>>> print(x is not y)
+True
+>>> print(x != y)
+False
+>>> print('banana' in x)
+True
+>>> 
