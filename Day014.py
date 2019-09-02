395ff401f205dadfed7e9c8d14ef579fd916ad46
غدا انسى <go090on@gmail.com>
2019-09-02 15:06:18 +0300

    Day014

---
+++ Day014.py
@@ -0,0 +1,28 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> thislist = ['A', 'B', 'C', 'D', 'E']
+>>> print(thislist[1:3])
+['B', 'C']
+>>> numbers = ['1', '2', '3', '4', '5']
+>>> print(numbers[1:4])
+['2', '3', '4']
+>>> thislist = ['apple', 'banana', 'cherry']
+>>> if 'apple' in thislist:
+	print('yes, "apple" is in the fruits list')
+
+	
+yes, "apple" is in the fruits list
+>>> thislist = ['Python']*4
+>>> print(thislist)
+['Python', 'Python', 'Python', 'Python']
+>>> thislist1 = ['Ahmad', 'Khlid', 'Omar']
+>>> thislist2 = ['Sara', 'Hind', 'Batool']
+>>> thislist3 = thislist1 + thislist2
+>>> print(thislist3)
+['Ahmad', 'Khlid', 'Omar', 'Sara', 'Hind', 'Batool']
+>>> numbers1 = ['1', '2', '3', '4', '5']
+>>> numbers2 = ['1.5', '2.5', '3.5', '4.5', '5.5']
+>>> numbers3 = numbers1 + numbers2
+>>> print(numbers3)
+['1', '2', '3', '4', '5', '1.5', '2.5', '3.5', '4.5', '5.5']
+>>> 
