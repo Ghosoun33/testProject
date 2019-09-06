a0dd8aabc55d461e0b3492a2ede9e0a761e6dde3
غدا انسى <go090on@gmail.com>
2019-09-06 14:59:41 +0300

    Day18

---
+++ Day18.py
@@ -0,0 +1,17 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list = ['Somaya', 'Maha', 'Asma']
+>>> list.append('Fatema')
+>>> print(list)
+['Somaya', 'Maha', 'Asma', 'Fatema']
+>>> list = ['Somaya', 'Maha', 'Asma', 'Fatema']
+>>> print(list[1])
+Maha
+>>> print(list[2:4])
+['Asma', 'Fatema']
+>>> list.remove('Maha')
+>>> print(list)
+['Somaya', 'Asma', 'Fatema']
+>>> print(len(list))
+3
+>>> 
---
+++ Day19.py
@@ -0,0 +1,9 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> tuple = ('java', 'python', 'swift')
+>>> if 'python' in tuple:
+	print('yes, "python" is in the tuple list')
+
+	
+yes, "python" is in the tuple list
+>>> 
