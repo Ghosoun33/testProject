53074faff620ae35e1ba44c7050d2b146077c9ed
غدا انسى <go090on@gmail.com>
2019-09-25 13:54:42 +0300

    Day37

---
+++ Day37.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:13:57) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> cars = ['Ford', 'Volvo', 'BMW']
+>>> print(cars)
+['Ford', 'Volvo', 'BMW']
+>>> x = cars[0]
+>>> print(x)
+Ford
+>>> cars[0] = 'Toyota'
+>>> print(cars)
+['Toyota', 'Volvo', 'BMW']
+>>> x = len(cars)
+>>> print(x)
+3
+>>> 
