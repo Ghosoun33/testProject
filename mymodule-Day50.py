4e11b65e90d7bfb2b1b1b45076400decf1a82341
غدا انسى <go090on@gmail.com>
2019-10-08 10:23:36 +0300

    mymodule-Day50

---
+++ mymodule-Day50.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def greeting(name):
+	print('Hello, ' + name)
+	import mymodule
+	mymodule.greeting('Jonathan')
+	person1 = {
+	'name': 'John',
+	'age': 36,
+	'country': 'Norway'
+	}
+	import mymodule
+	a = mymodule.person1['age']
+	print(a)
+	
