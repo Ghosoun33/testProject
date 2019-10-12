67be5d0760485f844620de3d6e016f9f737a1202
غدا انسى <go090on@gmail.com>
2019-10-12 15:14:12 +0300

    Day53-54

---
+++ Day53-54.py
@@ -0,0 +1,13 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import mymodule
+>>> import datetime
+>>> mymodule.add(8, 1)
+>>> mymodule.sub(4, 2)
+>>> mymodule.multl(6, 6)
+>>> mymodule.dividing(8, 2)
+
+>>> x = datetime.datetime.now()
+>>> print(x.strftime('%c'))
+Sat Oct 12 15:08:28 2019
+>>> 
