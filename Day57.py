22597386138ad501a715eea4efa1b459ac54b9a3
غدا انسى <go090on@gmail.com>
2019-10-15 11:02:18 +0300

    Day57

---
+++ Day57.py
@@ -0,0 +1,13 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import re
+>>> txt = 'The rain in Spain'
+>>> x = re.search('^The.*Spain$', txt)
+>>> if (x):
+	print('YES! We have a match!')
+else:
+	print('No match')
+
+	
+YES! We have a match!
+>>> 
