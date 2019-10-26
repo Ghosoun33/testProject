6d20b4311896e4b2444bf22c836a7240f9731c80
غدا انسى <go090on@gmail.com>
2019-10-26 13:43:04 +0300

    Day67-68

---
+++ Day67-68.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> first = input('Write your first name: ')
+Write your first name: 
+>>> second = input('Write your last name: ')
+Write your last name: 
+>>> print('Your first name: ', first, 'and your second name: ', second)
+Your first name:   and your second name:  
+>>> 
+>>> 
+>>> name =  'Ahmed Ali'
+>>> balance = 53.44
+>>> print('Dear {}, Your current balanec is {} $'.format(name, balance))
+Dear Ahmed Ali, Your current balanec is 53.44 $
+>>> 
